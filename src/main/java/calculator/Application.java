bbb2a5fccd967580ab63d081680af3d746b9b137
Kojongwoo <jongu010@gmail.com>
2025-10-16 22:08:22 +0900

    fix: resolve IllegalArgumentException by refactoring logic

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,7 @@ package calculator;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 class StringCalculator {
     int add(String text) {
@@ -16,11 +17,20 @@ class StringCalculator {
         // 커스텀 구분자
         if (text.startsWith("//")) {
             char customSeparator = text.charAt(2); // 커스텀 구분자는 // 뒤 문자
-            int newLineIndex = text.indexOf("\n"); // 줄바꿈 문자 인덱스 번호
-            String numberString = text.substring(newLineIndex + 1); // 줄바꿈 문자 다음 인덱스부터 문자열 반환
+//            int newLineIndex = text.indexOf("\n"); // 줄바꿈 문자 인덱스 번호
+            int newLineIndex = text.indexOf("\\n"); // \n 대신 \\n으로 작성해야 컴파일러는 첫번째 \, 두번째 \ 신호를 인식함.
+
+            // 만약 '\\n'을 찾지 못했다면 문제있는 입력으로 처리
+            if (newLineIndex == -1) {
+                throw new IllegalArgumentException("Custom separator format is invalid.");
+            }
+
+            String numberString = text.substring(newLineIndex + 2); // 줄바꿈 문자 다음 인덱스부터 문자열 반환
 
             // 커스텀 구분자 char -> String으로 형변환 후 자름
-            numbers = numberString.split(String.valueOf(customSeparator));
+            // numbers = numberString.split(String.valueOf(customSeparator));
+            // String의 split() 메소드가 구분자를 단순한 글자가 아닌 정규표현식으로 인식 -> Pattern.quote()로 해결
+            numbers = numberString.split(Pattern.quote(String.valueOf(customSeparator)));
 
         } else { // 쉼표(,) 또는 콜론(:)을 구분자로 가지는 경우
             String basicSeperator = text.replace(':', ','); // 기본 구분자 통일
@@ -37,11 +47,11 @@ class StringCalculator {
             try {
                 number = Integer.parseInt(s);
             } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("입력값에 숫자가 아닌 문자가 포함되어 있습니다.");
+                throw new IllegalArgumentException("no char. only number");
             }
 
             if (number < 0) {
-                throw new IllegalArgumentException("음수는 요구사항에 맞지 않는 입력값입니다.");
+                throw new IllegalArgumentException("negative number error");
             }
             sum += number;
         }
@@ -58,14 +68,14 @@ public class Application {
 
         int result;
 
-        System.out.println("덧셈할 문자열을 입력해주세요. ");
+        System.out.println("input String: ");
         String input = br.readLine();
 
         try {
             result = calculator.add(input);
-            System.out.println("결과 : " + result);
+            System.out.println("result : " + result);
         } catch (IllegalArgumentException e) {
-            System.out.println("에러: " + e.getMessage());
+            System.out.println("error: " + e.getMessage());
         }
     }
 }
